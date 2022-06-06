b1753c55f431eb7b1e1a479392f84ae5d91133dd
Artem K <akrainiev@betacom.com.pl>
2022-06-06 13:06:47 +0200

    Optimized styles

--- src/views/login/index.ts
+++ src/views/login/index.ts
@@ -1,15 +1,11 @@
 import '~/scss/index.scss'
 import { createApp } from 'vue'
 import index from './index.vue'
-import BalmUI from 'balm-ui'; // Official Google Material Components
-import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+
 
 const environment = document.querySelector('#environment')
 if (environment) {
-  const app = createApp(index)
-  app.use(BalmUI);
-  // @ts-ignore
-  app.use(BalmUIPlus);
+  const app = createApp(index);
   app.provide<Environment>('environment', JSON.parse(String(environment.textContent)))
   app.mount('#app')
 }
