5207e7f6de2a725421d880566ee7a0f92d530567
Artem <akrainiev@betacom.com.pl>
2022-06-13 15:15:00 +0200

    Fixed menu on mobile view

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="auth">
+  <div class="auth"
+       :class="{
+              'auth--show-mobile-menu': showMenu}"
+       style="overflow-x: hidden"
+  >
     <div class="top-bar">
     <span class="top-bar__actions">
 <!--      <a class="top-bar__action"-->
@@ -28,29 +32,34 @@
             <span>{{ menuItem.translation }}</span>
           </a>
         </nav>
-        <button class="header__menu-button"
-                tabindex="2"
+        <button class="header__menu-button waves-effect btn-flat"
+                tabindex="2" @click="toggleMobileMenu()"
         >
           <i class="material-icons">{{ showMenu ? 'close' : 'dehaze' }}</i>
         </button>
       </header>
       <nav class="mobile-menu" :class="{'mobile-menu--show': showMenu}">
         <div class="mobile-menu__item"
+             @click="onMenuAction(menuItem.title)"
              v-for="menuItem in menuItems">
           <i class="material-icons">{{ menuItem.icon }}</i>
           <span>{{ menuItem.translation }}</span>
         </div>
-        <hr>
-        <div class="mobile-menu__item mobile-menu__item--small">
-          <i class="material-icons">filter_b_and_w</i>
-          <span>High contrast </span>
-        </div>
-        <div class="mobile-menu__item mobile-menu__item--small"
-        >
-          <i class="material-icons">language</i>
-          <span>English</span>
-        </div>
+<!--        <hr>-->
+<!--        <div class="mobile-menu__item mobile-menu__item&#45;&#45;small">-->
+<!--          <i class="material-icons">filter_b_and_w</i>-->
+<!--          <span>High contrast </span>-->
+<!--        </div>-->
+<!--        <div class="mobile-menu__item mobile-menu__item&#45;&#45;small"-->
+<!--        >-->
+<!--          <i class="material-icons">language</i>-->
+<!--          <span>English</span>-->
+<!--        </div>-->
       </nav>
+      <div class="dimmer" 
+           @click="toggleMobileMenu()"
+           :class="{'dimmer--show': showMenu, 'dimmer--hidden': !showDimmer}">
+      </div>
       <div class="container auth__container" style="padding-top: 10vh">
         <section class="generic-auth-area">
           <div class="generic-auth-area__form-container">
@@ -92,11 +101,29 @@ export default defineComponent({
         icon: 'local_library',
         title: 'user-guide'
       }
-    ]
+    ],
+    showMenu: false,
+    showDimmer: false,
   }),
   methods: {
     onMenuAction(title: string) {
       window.location.href = useLogin().toAbsUrl(title);
+    },
+    
+    toggleMobileMenu(directChange?: boolean) {
+      if (directChange) {
+        this.showMenu = directChange;
+      } else {
+        this.showMenu = !this.showMenu;
+      }
+
+      if (!this.showMenu) {
+        setTimeout(() => {
+          this.showDimmer = false;
+        }, 250);
+      } else {
+        this.showDimmer = true;
+      }
     }
   }
 })
